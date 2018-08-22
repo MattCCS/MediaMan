71174583af11fa23dd8b491e004ace44f91a36bc
Matt Cotton <mcotton@racap.com>
2018-08-22 11:39:44 -0400

    (WIP) Begin crypto middleman impl

--- mediaman/core/crypto.py
+++ mediaman/core/crypto.py
@@ -1,4 +1,6 @@
 
+import tempfile
+
 from mediaman.services.abstract import service
 
 
@@ -7,6 +9,14 @@ class EncryptionMiddlewareService(service.AbstractService):
     def __init__(self, service):
         self.service = service
 
+    def encrypt(self, file_path):
+        tempfile_ref = tempfile.NamedTemporaryFile(mode="wb+")
+        with open(file_path, "rb") as infile:
+            # TODO: encrypt
+            tempfile_ref.write(b"ENCRYPT:" + infile.read())
+            tempfile_ref.seek(0)
+        return tempfile_ref
+
     def hash_function(self):
         return self.service.hash_function()
 
@@ -14,16 +24,18 @@ class EncryptionMiddlewareService(service.AbstractService):
         self.service.authenticate()
 
     def files(self):
-        return (self.decrypt(f) for f in self.service.files())
+        return self.service.files()
 
     def exists(self, file_id):
         return self.service.exists(file_id)
 
     def put(self, source_file_path, destination_file_path):
-        return self.service.put(
-            self.encrypt(source_file_path),
-            destination_file_path
-        )
+        with self.encrypt(source_file_path) as encrypted_tempfile:
+            encrypted_file_path = encrypted_tempfile.name
+            return self.service.put(
+                encrypted_file_path,
+                destination_file_path
+            )
 
     def get(self, file_id):
         return self.decrypt(self.service.get(file_id))
---
+++ mediaman/test/crypto_local_put.py
@@ -0,0 +1,15 @@
+
+import uuid
+
+from mediaman.core import crypto
+from mediaman.services import loader
+
+service = loader.load_local()
+service = crypto.EncryptionMiddlewareService(service)
+
+random_filename = str(uuid.uuid4())
+
+print(service.exists(random_filename))
+result = service.put("mediaman/test.txt", random_filename)
+print(result, result.id())
+print(service.exists(result.id()))
