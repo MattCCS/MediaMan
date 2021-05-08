4ae8e1db3e0f18780dac0e3f490d22121848d188
Matt <matt@black-mac-mini.local>
2021-05-08 09:11:09 -0400

    - (WIP) Fix some setup.py bugs, skip bash script

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 PyPI setup file
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    name = 'mediaman',
-    packages = ['mediaman'],
-    version = '0.0.1',
+    name='mediaman',
+    packages=find_packages(exclude='mm'),
+    version='0.0.2',
     author='Matt Cotton',
     author_email='matthewcotton.cs@gmail.com',
     url='https://github.com/MattCCS/MediaMan',
@@ -18,10 +18,15 @@ setup(
     long_description_content_type='text/markdown',
     classifiers=["Programming Language :: Python :: 3"],
 
-    entry_points={
-        'scripts': [
-            'mm=mm',
-        ],
-    },
-)
+    install_requires=[
+        'cryptography',
+        'PyYAML',
+        'xxhash',
+    ],
 
+    entry_points={
+        'console_scripts': [
+            'mm=mediaman.interfaces.cli:main'
+        ]
+    }
+)
